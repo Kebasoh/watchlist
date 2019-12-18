f139ed89d3bc0f39289e342ddf1eeb2ac2873346
Kebasoh <ongatikebaso@gmail.com>
2019-12-18 13:52:07 +0300

    msm

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app import app
 from .request import get_movies
+from .request import get_movies,get_movie
+
 
 # Views
 @app.route('/')
@@ -21,5 +23,7 @@ def movie(movie_id):
     '''
     View movie page function that returns the movie details page and its data
     '''
-    return render_template('movie.html',id = movie_id)
+    movie = get_movie(id)
+    title = f'{movie.title}'
+    return render_template('movie.html',title = title,movie = movie)
 
